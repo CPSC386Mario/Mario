68515336d4b44cecd932280b30de770b67d0f602
andrew96nguyen <31872897+andrew96nguyen@users.noreply.github.com>
2018-10-29 21:23:31 -0700

    Basic Mario movement (left and right)

--- game_functions.py
+++ game_functions.py
@@ -4,6 +4,7 @@ import pygame
 
 def update_screen(screen, mario):
     screen.fill((0, 255, 255))
+    mario.update()
     mario.blitme()
     pygame.display.flip()
 
@@ -16,3 +17,13 @@ def check_events(screen, mario):
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_q:
                 sys.exit()
+            elif event.key == pygame.K_LEFT:
+                mario.moving_left = True
+            elif event.key == pygame.K_RIGHT:
+                mario.moving_right = True
+
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                mario.moving_left = False
+            elif event.key == pygame.K_RIGHT:
+                mario.moving_right = False
--- mario.py
+++ mario.py
@@ -7,6 +7,7 @@ class Mario(Sprite):
     def __init__(self, screen):
         super(Mario, self).__init__()
         self.screen = screen
+        self.screen_rect = screen.get_rect()
 
         self.small_mario = []
         self.image = pygame.Surface((16, 16))
@@ -17,6 +18,10 @@ class Mario(Sprite):
         self.image = pygame.transform.scale(self.image, (50, 50))
         self.rect = self.image.get_rect()
 
+        # Movement Flags
+        self.moving_left = False
+        self.moving_right = False
+
         for i in range(0, 13):
             temp_img = pygame.Surface((16, 16))
             temp_img.set_colorkey((0, 0, 0))
@@ -26,8 +31,14 @@ class Mario(Sprite):
 
         self.image = self.small_mario[0]
         self.rect = self.image.get_rect()
-        self.rect.x = 0
-        self.rect.y = 0
+        self.rect.x = self.screen_rect.centerx
+        self.rect.y = self.screen_rect.centery + 100
+
+    def update(self):
+        if self.moving_right:
+            self.rect.x += 5
+        elif self.moving_left:
+            self.rect.x -= 5
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
